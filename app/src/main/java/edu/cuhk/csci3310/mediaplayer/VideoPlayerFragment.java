ff7d997cc775de47ac0ae4c53c0feff336fdc477
hinericli <hin.ericli@gmail.com>
2024-05-04 19:00:39 +0800

    Provide some getters in VideoPlayerFragment.java

--- app/src/main/java/edu/cuhk/csci3310/mediaplayer/VideoPlayerFragment.java
+++ app/src/main/java/edu/cuhk/csci3310/mediaplayer/VideoPlayerFragment.java
@@ -3,20 +3,29 @@ package edu.cuhk.csci3310.mediaplayer;
 import android.app.PictureInPictureParams;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.media3.common.C;
 import androidx.media3.common.MediaItem;
+import androidx.media3.common.MimeTypes;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.slider.Slider;
+import com.google.common.collect.ImmutableList;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class VideoPlayerFragment extends Fragment {
     private SharedPreferences mPreferences;
@@ -45,19 +54,18 @@ public class VideoPlayerFragment extends Fragment {
         // player stores the Exoplayer
         player = new ExoPlayer.Builder(getContext()).build();
         // videoUri stores the url/file of the video
-        String videoUri = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4";
-        // mediaItem stores the media for player (video)
-        MediaItem mediaItem = MediaItem.fromUri(videoUri);
-
-        //MediaItem mediaItem = new MediaItem.Builder().setUri("/storage/emulated/0/Download/WeAreGoingOnBullrun.mp4");
+        MediaItem mediaItem = getMediaItem();
         // Attach player to the view
         playerView.setPlayer(player);
         // Set the media item
         player.setMediaItem(mediaItem);
         // Prepare player
         player.prepare();
-        // Resume the video progress after orientation changes or expected crashes of apps
-        player.seekTo(mPreferences.getLong(ARG_CONTENT_POSITION, 0));
+        // Resume the video progress after orientation changes,
+        Log.d("ContentDuration", Long.toString(getVideoLength(getVideoUri(mediaItem))));
+        if (mPreferences.getLong(ARG_CONTENT_POSITION, 0) <= getVideoLength(getVideoUri(mediaItem)))
+            player.seekTo(mPreferences.getLong(ARG_CONTENT_POSITION, 0));
+
         // Play the video
         player.play();
 
@@ -114,5 +122,64 @@ public class VideoPlayerFragment extends Fragment {
         SharedPreferences.Editor editor = mPreferences.edit();
         editor.putLong(ARG_CONTENT_POSITION, player.getContentPosition());
         editor.apply();
+    }
+
+    private MediaItem getMediaItem() {
+        /*
+        String videoUri = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4";
+        // mediaItem stores the media for player (video)
+        MediaItem mediaItem = MediaItem.fromUri(videoUri);
+        */
+
+        File downloadFolder = requireContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        Log.d("downloadFolder", downloadFolder.toString());
+        File[] videoList = downloadFolder.listFiles();
+        /*
+        // / ---- Subtitle ----
+        MediaItem.SubtitleConfiguration subtitle =
+                new MediaItem.SubtitleConfiguration.Builder(Uri.parse(videoList[2].getAbsolutePath().toString()))
+                        .setMimeType(MimeTypes.APPLICATION_TTML)
+                        .setLanguage("en-US")
+                        .setSelectionFlags(C.SELECTION_FLAG_FORCED)
+                        .build();
+        MediaItem mediaItem = new MediaItem.Builder()
+                .setUri(videoList[2].getAbsolutePath().toString())
+                .setSubtitleConfigurations(ImmutableList.of(subtitle))
+                .build();
+        */
+        MediaItem mediaItem = new MediaItem.Builder()
+                .setUri(videoList[2].getAbsolutePath().toString())
+                .build();
+        return mediaItem;
+    }
+
+    private long getVideoLength(Uri uri) {
+        /*
+            Input: Uri
+            Output: length of the video at uri (in long)
+         */
+        long timeInMillisec = 0;
+        try {
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(getContext(), uri);
+            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            if (time != null) timeInMillisec = Long.parseLong(time);
+
+            retriever.release();
+
+        } catch (Exception e) {
+            Log.e("getVideoLength", e.toString());
+        }
+        return timeInMillisec;
+    }
+
+    private Uri getVideoUri(MediaItem mediaItem) {
+        // getVideoLength(mediaItem.localConfiguration.uri): Uri of the video
+        if (mediaItem.localConfiguration != null) {
+            return mediaItem.localConfiguration.uri;
+        } else {
+            Log.e("getVideoUri", "Unable to retrieve video uri!");
+            return Uri.parse("");
+        }
     }
 }
