589656519b6b53759935bd3d0050408a13d00150
hinericli <hin.ericli@gmail.com>
2024-05-07 13:20:17 +0800

    Merged videoplayer branch with filemanager branch

--- app/src/main/java/edu/cuhk/csci3310/mediaplayer/ui/VideoPlayerFolder/VideoPlayer.java
+++ app/src/main/java/edu/cuhk/csci3310/mediaplayer/ui/VideoPlayerFolder/VideoPlayer.java
@@ -1,24 +1,27 @@
 package edu.cuhk.csci3310.mediaplayer.ui.VideoPlayerFolder;
 
+import android.app.PictureInPictureParams;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.media3.common.MediaItem;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
 import com.google.android.material.slider.Slider;
+
+import java.io.File;
 
 import edu.cuhk.csci3310.mediaplayer.R;
 
@@ -26,21 +29,28 @@ public class VideoPlayer extends AppCompatActivity {
 
     private SharedPreferences mPreferences;
     private String sharedPrefFile = "edu.cuhk.csci3310.mediaplayer";
-    private final String CONTENT_POSITION_KEY = "content_position";
+    private final String ARG_CONTENT_POSITION = "content_position";
+    private PlayerView playerView;
     private ExoPlayer player;
+    private Slider volumeSlider;
 
     ImageView shareBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove the top bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         setContentView(R.layout.activity_video_player);
         Context context = this;
         // For storing video playback info
         mPreferences = context.getSharedPreferences(sharedPrefFile, context.MODE_PRIVATE);
 
+
         // ---- VIDEO PLAYBACK ----
         // playerView stores the view of the video player
-        PlayerView playerView = findViewById(R.id.player_view);
+        playerView = findViewById(R.id.player_view);
         // player stores the Exoplayer
         player = new ExoPlayer.Builder(this).build();
         // videoUri stores the url/file of the video
@@ -54,8 +64,10 @@ public class VideoPlayer extends AppCompatActivity {
         player.setMediaItem(mediaItem);
         // Prepare player
         player.prepare();
-        // Resume the video progress after orientation changes or expected crashes of apps
-        player.seekTo(mPreferences.getLong(CONTENT_POSITION_KEY, 0));
+        // Resume the video progress after orientation changes,
+        Log.d("ContentDuration", Long.toString(getVideoLength(getVideoUri(mediaItem))));
+        if (mPreferences.getLong(ARG_CONTENT_POSITION, 0) <= getVideoLength(getVideoUri(mediaItem)))
+            player.seekTo(mPreferences.getLong(ARG_CONTENT_POSITION, 0));
         // Play the video
         player.play();
 
@@ -76,7 +88,7 @@ public class VideoPlayer extends AppCompatActivity {
         });
 
         // ---- Volume Slider ----
-        Slider volumeSlider = findViewById(R.id.volumeSlider);
+        volumeSlider = findViewById(R.id.volumeSlider);
         volumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
             @Override
             public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
@@ -98,6 +110,14 @@ public class VideoPlayer extends AppCompatActivity {
                 }
             }
         });
+
+        // ---- PIP ----
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            setPictureInPictureParams(new PictureInPictureParams.Builder()
+                    .setAutoEnterEnabled(true)
+                    .build());
+        }
+
     }
 
     @Override
@@ -122,9 +142,52 @@ public class VideoPlayer extends AppCompatActivity {
         // Release the player
         player.release();
     }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode);
+        if (isInPictureInPictureMode) {
+            playerView.setUseController(false);
+            volumeSlider.setVisibility(View.INVISIBLE);
+        } else {
+            playerView.setUseController(true);
+        }
+        player.play();
+    }
+
     private void saveVideoProgress() {
         SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putLong(CONTENT_POSITION_KEY, player.getContentPosition());
+        editor.putLong(ARG_CONTENT_POSITION, player.getContentPosition());
         editor.apply();
+    }
+
+    private long getVideoLength(Uri uri) {
+        /*
+            Input: Uri
+            Output: length of the video at uri (in long)
+         */
+        long timeInMillisec = 0;
+        try {
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(getApplicationContext(), uri);
+            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            if (time != null) timeInMillisec = Long.parseLong(time);
+
+            retriever.release();
+
+        } catch (Exception e) {
+            Log.e("getVideoLength", e.toString());
+        }
+        return timeInMillisec;
+    }
+
+    private Uri getVideoUri(MediaItem mediaItem) {
+        // getVideoLength(mediaItem.localConfiguration.uri): Uri of the video
+        if (mediaItem.localConfiguration != null) {
+            return mediaItem.localConfiguration.uri;
+        } else {
+            Log.e("getVideoUri", "Unable to retrieve video uri!");
+            return Uri.parse("");
+        }
     }
 }
